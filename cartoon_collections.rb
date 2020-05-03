eb539a6ef5063c57545099391f377785c5b3e8f0
Iona <ionabrabender@gmail.com>
2020-05-03 00:53:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,4 +31,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
     planeteer_calls.find { |call| call == ("Earth!") || ("Wind!") || ("Fire!") || ("Water!") || ("Heart!")}
+  end
 end
