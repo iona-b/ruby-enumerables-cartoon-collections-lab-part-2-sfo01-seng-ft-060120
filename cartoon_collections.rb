f58f8bd0ea69a90991ce1a5e571d35b2ac52e9e2
Iona <ionabrabender@gmail.com>
2020-05-03 00:56:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
@@ -29,6 +30,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-
+    binding.pry
     planeteer_calls.find { |call| call == ("Earth!") || ("Wind!") || ("Fire!") || ("Water!") || ("Heart!")}
 end
