ec6ae36294282c5f2651bce4da07453654d5211b
Iona <ionabrabender@gmail.com>
2020-05-03 00:48:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,5 +30,5 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 
-    planeteer_calls.find do |planet| planet == ("Earth!" || "Wind!"|| "Fire!" || "Water!" || "Heart!")
+    planeteer_calls.find {|planet| planet == ("Earth!" || "Wind!"|| "Fire!" || "Water!" || "Heart!")}
 end
